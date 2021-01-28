47ed900623af1508556a822da7548e252744b0d5
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-01-28 23:28:26 +0100

    first commit on branch

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ToDolist,Item
+from .models import ToDoList,Item
 
 # Create your views here.
-def index(response, id):
-    ls = ToDolist.objects.get(id =id)
+def index(response, id = 1):
+    ls = ToDoList.objects.get(id =id)
     return render(response, 'main/base.html', {})
 
 def home(response):
-    return render(response, 'home/base.html', {})
+    return render(response, 'main/home.html', {})
