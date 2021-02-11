3ab11e05baa199ef2389fc57997c639e6e076947
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-02-11 21:38:46 +0100

    Pushing boostrap changes

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from  . import  views
 
-urlpatterns =[
+urlpatterns = [
     path("<int:id>", views.index, name = "index"),
     path("", views.home, name = "home"),
     path("create/", views.create, name = "create"),
