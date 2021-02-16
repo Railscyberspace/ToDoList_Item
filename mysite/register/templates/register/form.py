bb600357cf82c744a319f24990a51bfb083ff839
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-02-16 15:13:09 +0100

    commiting  changes from url.py

--- mysite/register/templates/register/form.py
+++ mysite/register/templates/register/form.py
@@ -2,6 +2,14 @@
 from django.models  import models
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+#from forms import RegisterForm
 
 class RegisterForm(UserCreationForm):  
     Email = models.EmailField()
+    
+    
+    
+    
+    
+    
+    
